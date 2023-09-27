9a97704e78f31910890292b21a91621c29061eb7
msahrana <msahrana@gmail.com>
2023-09-27 22:55:04 +0600

    update image part again

--- src/Components/Header/Navbar/Logo.jsx
+++ src/Components/Header/Navbar/Logo.jsx
@@ -2,7 +2,7 @@
 const Logo = () => {
     return (
         <div>
-           <img src="/public/Resources/Logo.png" alt="" />
+           <img src="/Resources/Logo.png" alt="" />
         </div>
     );
 };
