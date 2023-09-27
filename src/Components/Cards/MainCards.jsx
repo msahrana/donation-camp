f7284a0a545cdd00871488ac0ed0d7847e69e7db
msahrana <msahrana@gmail.com>
2023-09-27 12:49:34 +0600

    all color section of my website is update

--- src/Components/Cards/MainCards.jsx
+++ src/Components/Cards/MainCards.jsx
@@ -11,7 +11,7 @@ const MainCards = ({card}) => {
             <div className="card w-[312px] h-[283px] bg-slate-300 rounded" style={{backgroundColor: card_bg}}>
                 <img src={image} alt="" />
                 <div className="mx-2 mt-2">
-                    <h1 className="font-bold w-auto p-2" style={{color: text_color, backgroundColor: category_bg}}>{category}</h1>
+                  <button style={{color: text_color, backgroundColor: category_bg}} className="py-1 px-2 font-bold rounded">{category}</button>
                     <h1 className="text-xl font-semibold mt-3" style={{color: text_color}}>{title}</h1>
                 </div>
             </div>
--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,8 +1,12 @@
 
 const Statistics = () => {
+
+  
+
+
     return (
         <div>
-            Statistics 
+         
         </div>
     );
 };
