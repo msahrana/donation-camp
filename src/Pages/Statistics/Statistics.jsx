d789a5103ff5590a61a00d3d2fad13a376b94546
msahrana <msahrana@gmail.com>
2023-09-27 22:10:57 +0600

    statistics part is added

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -1,5 +1,30 @@
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react-hooks/exhaustive-deps */
+
+import { useEffect } from "react";
+import { useState } from "react";
+import { Pie, PieChart } from "recharts";
 
 const Statistics = () => {
+
+  const [donate, setDonate] = useState()
+
+  useEffect(()=> {
+    const donationItem = JSON.stringify(localStorage.getItem('donation'))
+    setDonate(donationItem)
+    console.log(donate)
+  },[])
+
+  const myDonate = (setDonate /12)*100;
+  const remaning = 100 - myDonate;
+
+  const data = [
+    { name: 'myDonation', value: 'myDonate' },
+    { name: 'totalDonation', value: 'remaning' },
+  ]
+
   return (
     <div>
       Statistics 
